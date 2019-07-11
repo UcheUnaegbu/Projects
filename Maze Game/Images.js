bba0ebb041eefe5f2eec70835450da540030171f
UcheUnaegbu <47509276+UcheUnaegbu@users.noreply.github.com>
2019-07-11 02:46:05 -0400

    Datto Maze Game Files

---
+++ Maze Game/Images.js
@@ -0,0 +1,31 @@
+//defines the Datto image at the end of the maze
+let endImage = new Image();
+endImage.src = 'logo-datto-square.png';
+
+//okta image
+let oktaImage = new Image();
+oktaImage.src = 'okta.png';
+
+//slack image
+let slackImage = new Image();
+slackImage.src = 'slack.jpg';
+
+//adp image
+let adpImage = new Image();
+adpImage.src = 'adplogo.jpg';
+
+//concur image
+let concurImage = new Image();
+concurImage.src = 'concur.jpg';
+
+//pingboard image
+let pingboardImage = new Image();
+pingboardImage.src = 'pingboard.png';
+
+//ioffice image
+let iOfficeImage = new Image();
+iOfficeImage.src = 'ioffice.png';
+
+//homepool image
+let homepoolImage = new Image();
+homepoolImage.src = 'homepool.jpeg';
---
+++ Maze Game/InfoText.js
@@ -0,0 +1,44 @@
+var oktaText =
+    "Okta <br><br>" +
+    "You can access all of your applications " +
+    "by going to datto.okta.com . Some of these " +
+    "applications are also discussed at other " +
+    "checkpoints within this maze.";
+
+var adpText =
+    "ADP <br><br>" +
+    "You want get your check, right? ADP is an " +
+    "application used to set up your direct deposit " +
+    "and where your timesheets are completed.";
+
+var pingboardText =
+    "Pingboard <br><br>" +
+    "Information Overload! Pingboard  is an application " +
+    "that holds info of all employees. Names, Pictures, " +
+    "Birthdays, Positions, etc. It even has an org chart " +
+    "showing the chain of command like a family tree.";
+
+var homepoolText =
+    "Homepool<br><br>" +
+    "You think it, You search. Homepool contains all of the information " +
+    "about the company on it. You can use the search bar to " +
+    "find almost anything.";
+
+var concurText =
+    "Concur<br><br>" +
+    "Uber everywhere! Concur is an application that " +
+    "handles all your travel expenses for company trips.";
+
+var iOfficeText =
+    "iOffice<br><br>" +
+    "Lost? iOffice is a tool that shows the layout of every " +
+    "Datto office space. So finding the kitchens, game rooms," +
+    " and bathrooms are easy. You can also search room names or " +
+    "employees names to find their desks.";
+
+var slackText =
+    "Slack<br><br>" +
+    "Just slack me. Slack is a tool used to communicate with" +
+    " everyone working at Datto. They have channels/group chats " +
+    "for each individual office, new hires, inclusion intiatives," +
+    "and even leftover food.";
